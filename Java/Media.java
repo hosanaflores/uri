1dacb8171726c74c33cd8d779853f93d6e7320ba
Hosana Silvino <hosana.flores@sptech.school>
2025-01-18 16:11:30 -0300

    Exercício Média de Notas

---
+++ Java/Media.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Media {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+
+        double soma = (a * 3.5) + (b * 7.5);
+        double MEDIA = soma / 11.0;
+
+        System.out.printf("MEDIA = %.5f\n",MEDIA);
+
+        scanner.close();
+    }
+}
