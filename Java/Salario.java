61cfcce3c34fd0141669035001b0936e046b2d06
Hosana Silvino <hosana.flores@sptech.school>
2025-01-19 13:40:43 -0300

    Exercício Salário das horas trabalhadas sem bônus

---
+++ Java/Salario.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Salario {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int numeroFuncionario = in.nextInt();
+        int horasTrabalhadas = in.nextInt();
+        double valorHora = in.nextDouble();
+        double salario = valorHora * horasTrabalhadas;
+
+        System.out.println("NUMBER = "+numeroFuncionario);
+        System.out.printf("SALARY = U$ %.2f", salario);
+
+        in.close();
+
+    }
+    
+}
